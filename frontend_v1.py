c223c4069a44efc8304e84191f4041db31c9b235
ikarezina <chiminyoongi10@gmail.com>
2024-10-01 08:06:57 +0300

    Frontend 2.0

--- frontend_v1.py
+++ frontend_v1.py
@@ -1,12 +1,10 @@
-
 from flask import Flask, render_template, request
 import requests
-
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
 
 @app.route("/send_data", methods=["POST"])
 def send_data():
@@ -17,6 +15,15 @@ def send_data():
         return "Data sent successfully", 200
     else:
         return "Failed to send data to server", 500
+
+@app.route("/get_data", methods=["POST"])
+def get_data():
+    # Send request to backend to get data
+    response = requests.post("http://localhost:5001/get_data")
+    if response.status_code == 200:
+        return response.text, 200
+    else:
+        return "Failed to get data from server", 500
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
